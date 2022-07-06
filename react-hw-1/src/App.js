7b468888c3287dd9590b531bb95cfaaa11a206bb
FlyStr1ker <Ketraofsaman@gmail.com>
2022-07-06 12:26:37 +0300

    add project file

---
+++ react-hw-1/src/App.js
@@ -0,0 +1,12 @@
+import './index.css';
+import Google from './project_files/Google';
+
+function App() {
+  return (
+    <div className="App">
+       <Google/>
+    </div>
+  );
+}
+
+export default App;
